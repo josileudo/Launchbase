3caf5c99b633fc908d16350dd2d19ff5a9dfbd0a
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-02-09 15:17:38 -0300

    desafios 3.1 concluido

---
+++ Backend/challenges/public/scripts.js
@@ -0,0 +1,28 @@
+const modalOverlay = document.querySelector(".modal-overlay")
+const modal = document.querySelector(".modal")
+const cards = document.querySelectorAll(".card")
+
+for (let card of cards) {
+  card.addEventListener("click", function(){
+    const pageId = card.getAttribute("id")
+    modalOverlay.classList.add("active")
+    modalOverlay.querySelector("iframe").src = `https://blog.rocketseat.com.br/${pageId}` 
+  })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+  modalOverlay.classList.remove("active")
+  modal.querySelector("iframe").src = ""
+  modal.classList.remove("maximize")
+})
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+  if (modal.classList.contains("maximize")){
+    modal.classList.remove("maximize")
+  } else {
+    modal.classList.add("maximize")
+  }
+})
+
+
+
