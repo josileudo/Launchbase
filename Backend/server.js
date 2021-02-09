291927caf0d5153270daa87e0c4eb8fcb8df8011
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-02-09 17:41:07 -0300

    passando dados do Back para o Front

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require ("express") //está indo buscar dentro do node_modules
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 server.set("view engine", "njk")
@@ -15,7 +16,7 @@ server.get("/", function(req, res){
 })
 
 server.get("/portfolio", function(req, res){
-  return res.render("portfolio")
+  return res.render("portfolio", {items: videos})
 })
 
 server.listen(5000, function(){ //ele está ounvido uma porta 
