6d8b95ac99cc51b037c1daba2300c01579dbf03e
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-02-09 15:47:58 -0300

    criação dos dados

--- Backend/data.js
+++ Backend/data.js
@@ -1,3 +1,38 @@
-const test = "hello word"
-
-console.log(test)
+module.exports = [
+  {
+    id: "GykTLqODQuU",
+    title: "Formulário animado com JS e CSS Animation | Mayk Brito", 
+    duration: "57 Min",
+    price: "Free"
+  },
+  {
+    id: "HN1UjzRSdBk",
+    title: "Desevendando o CSS Grid na Prática | Mayk Brito", 
+    duration: "36 Min",
+    price: "Free"
+  },
+  {
+    id: "vqrjFnq3-uo",
+    title: "Criando Player de áudio | Mayk Brito", 
+    duration: "105 Min",
+    price: "Free"
+  },
+  {
+    id: "2alg7MQ6_sI",
+    title: "Desvendando Git na prática | Mayk Brito", 
+    duration: "33 Min",
+    price: "Free"
+  },
+  {
+    id: "ghTrp1x_1As",
+    title: "O que é API REST e RESTFUL? | Mayk Brito", 
+    duration: "33 Min",
+    price: "Free"
+  },
+  {
+    id: "GSqR2i-Pq6o",
+    title: "Desvendando a variável THIS | Mayk Brito", 
+    duration: "48 Min",
+    price: "Free"
+  },
+]
