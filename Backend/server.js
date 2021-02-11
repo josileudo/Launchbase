019ac0d7686d8e79cc5183474ab555bf827cc0d5
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-02-11 10:06:13 -0300

    aulizando a página sobre

--- Backend/server.js
+++ Backend/server.js
@@ -8,11 +8,24 @@ server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-  express: server
+  express: server,
+  autoescape:false
 })
 
 server.get("/", function(req, res){
-  return res.render("about")
+  const about= {
+    avatar_url: 'https://avatars2.githubusercontent.com/u/48743781?s=400&u=b3328af9a6ad91510c9dc3dc2eebac65a3d5047a&v=4',
+    name: 'Josileudo Rodrigues',
+    role: 'Aluno - Rocketseat',
+    description: `Programador Web Júnior, focado no aprendizado contínuo. Aluno <a href="https://rocketseat.com.br"
+    target="blanck">Rocketseat</a>`,
+    links: [
+      {name: 'Twitter' , url: `https://twitter.com/JosileudoRodri1`},
+      {name: 'Github' , url: `https://github.com/josileudo`},
+      {name: 'Linkedin' , url: `https://www.linkedin.com/in/josileudo-rodrigues-a83a3b138`}
+    ]
+  }
+  return res.render("about", {about})
 })
 
 server.get("/portfolio", function(req, res){
