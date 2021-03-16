ea965eb4afb51842a063686bd5d2398bab3a70c1
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-03-16 16:22:35 -0300

    correct route using the loop id

--- Projects/Foodfy/public/scripts.js
+++ Projects/Foodfy/public/scripts.js
@@ -3,21 +3,7 @@ const modalOverlay = document.querySelector(".modal-overlay")
 
 for (let card of cards){
   card.addEventListener("click", function(){
-    const imgId = card.getAttribute("id")
-    const title = card.querySelector(".card__infor-content").innerHTML
-    const author = card.querySelector(".card__autor-content")
-
-    modalOverlay.classList.add("active")
-    modalOverlay.querySelector("img").src = `assets/${imgId}`
-
-    modalOverlay.querySelector(".modal_title").innerHTML = title
-    modalOverlay.querySelector(".infor__author").innerHTML = author
+    const recipe = card.getAttribute("id")
+    window.location.href = `/recipes/${recipe}`
   })
 }
-
-document.querySelector(".close-modal").addEventListener("click", function(){
-  modalOverlay.classList.remove("active")
-  modalOverlay.querySelector("img").src = ""
-  modalOverlay.querySelector(".modal_title").innerHTML = ""
-  modalOverlay.querySelector(".infor_author")
-})
